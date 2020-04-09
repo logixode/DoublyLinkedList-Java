7b0966fe82d2e5c54b7797478ad60a9827e93642
logixode <rohmad1230@gmail.com>
2020-04-09 09:33:20 +0700

    edit documentation

--- src/main/java/com/logixode/rk/doublylinkedlist/Main.java
+++ src/main/java/com/logixode/rk/doublylinkedlist/Main.java
@@ -1,10 +1,6 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.logixode.rk.doublylinkedlist;
-/** Class utama dari package CircularLinkedList
+
+/** Class utama dari package DoublyLinkedList
  * @author Rohmad Kurniadi
  */
 public class Main {
